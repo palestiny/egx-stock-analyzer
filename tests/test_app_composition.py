26702b5944f2beec9e6050bf75e929eaa0905a9d
palestiny <153906782+palestiny@users.noreply.github.com>
2026-09-20 00:00:34 +0300

    test(m45): update runtime composition fixture

--- tests/test_app_composition.py
+++ tests/test_app_composition.py
@@ -34,6 +34,7 @@ class FakeRuntime:
         self.deliver_alert_by_symbol = object()
         self.automatic_workflow_recovery = None
         self.get_scheduled_workflow_executions = object()
+        self.get_scheduled_workflow_execution_history = object()
         self.application_runtime = FakeApplicationRuntime(
             result_store,
             run_by_symbol=object(),
